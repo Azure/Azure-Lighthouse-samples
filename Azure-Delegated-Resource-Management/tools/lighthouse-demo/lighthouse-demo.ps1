549e8aacdeadf078118143ad50cf25a99883e8e7
Isaiah Williams <isaiah.williams@microsoft.com>
2019-11-02 23:37:40 -0500

    Updating the demo script

--- Azure-Delegated-Resource-Management/tools/lighthouse-demo/lighthouse-demo.ps1
+++ Azure-Delegated-Resource-Management/tools/lighthouse-demo/lighthouse-demo.ps1
@@ -2,6 +2,8 @@
 
 $MspTenant = "insert your managing tenantId here"
 
+$subs = Get-AzSubscription
+
 $ManagedSubscriptions = Search-AzGraph -Query "ResourceContainers | where type == 'microsoft.resources/subscriptions' | where tenantId != '$($mspTenant)' | project name, subscriptionId, tenantId" -subscription $subs.subscriptionId
 
 Search-AzGraph -Query "where type =~ 'Microsoft.Storage/storageAccounts' | project name, location, subscriptionId, tenantId, properties.supportsHttpsTrafficOnly" -subscription $ManagedSubscriptions.subscriptionId | convertto-json
