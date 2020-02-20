524aca74cab331ecedbe969c4d8f4c2a4ff2e576
Serena Zheng <serenaz@users.noreply.github.com>
2020-02-20 15:10:24 -0800

    Updated script to address comments

--- Azure-Delegated-Resource-Management/tools/check-user-access/check-user-access.ps1
+++ Azure-Delegated-Resource-Management/tools/check-user-access/check-user-access.ps1
@@ -1,32 +1,74 @@
-# Scenario: As a customer, I need to validate I have the appropriate permissions (Owner) on the subscriptions/resource groups I want to delegate
+# Displays the current user's Owner permissions on subscriptions
+function Get-AzSubOwnership {
+    
+    [cmdletbinding()]
+    param($SubscriptionId, $SubscriptionName) 
 
-$subid = "insert SubscriptionId here"
-
-$context = Get-AzContext
-Write-Host -nonewline "The current user ($($context.Account.Id)) "
-
-# Select subscription if current user has access
-try
-{
-    select-azsubscription -subscriptionId $subid -ErrorAction Stop > $null
-}
- 
-catch
-{
-    Write-Output "does not have access, or the SubscriptionId ($subid) is invalid."
-    exit
-}
-
-# Get user's roledefintions 
-$roles = Get-AzRoleAssignment -SignInName $context.Account.Id -ExpandPrincipalGroups 
-
-# Check if the user is an owner of the subscription
-foreach($role in $roles)
-{
-    if ($role.RoleDefinitionName -eq "Owner") {
-        Write-Output "is an owner."
+    # Check if Az.Accounts and Az.Resources modules are installed.
+    if (!(Get-Module "Az.Accounts") -or !(Get-Module "Az.Resources")) {
+        Write-Output "Please install Az.Accounts and Az.Resources modules."
         exit
     }
+
+    # Get Azure connection context for current user.
+    $context = Get-AzContext
+    Write-Host -nonewline "The current user ($($context.Account.Id)) "
+
+    # Get new access token.
+    # $azureRmProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+    # $profileClient = [Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient]::new($azureRmProfile)
+    # $token = $profileClient.AcquireAccessToken($context.Subscription.TenantId)
+    # https://github.com/Azure/azure-powershell/issues/7752
+
+    # Get all subscriptions user can access.
+    $subs = Get-AzSubscription
+    if (!$subs) {
+        Write-Output "does not have access to any subscriptions."
+        exit
+    }
+
+    # If specified as param, select subscription.
+    if ($SubscriptionId) 
+    {
+        $subs = $subs | where-object {$_.Id -eq $SubscriptionId}
+
+    }
+    elseif ($SubscriptionName)
+    {
+        $subs = $subs | where-object {$_.Name -eq $SubscriptionName}
+    }
+    if (!$subs) {
+        Write-Output "does not have access to the specified subscription, or the parameter is invalid."
+        exit
+    }
+    
+    # Display if user is Owner of subscription(s).
+    $n = 0
+    $output = foreach($sub in $subs)
+    {
+        select-azsubscription -SubscriptionId $sub.SubscriptionId > $null # Takes too much time!
+        
+        $roles = Get-AzRoleAssignment -SignInName $context.Account.Id -ExpandPrincipalGroups
+
+        foreach($role in $roles){
+            if ($role.RoleDefinitionName -eq "Owner") {
+                $n++
+                [pscustomobject]@{
+                    SubscriptionName = $sub.Name;
+                    SubscriptionId = $sub.Id;
+                    DisplayName = $role.DisplayName;
+                }
+            }
+        }
+        
+    }
+    if ($n -eq 0) {
+        Write-Output "is not an owner of any subscriptions."
+    }
+    else{
+        Write-Output "is an Owner of:"
+        $output | Format-Table
+    }
+    
 }
 
-Write-Output "is not an owner."
