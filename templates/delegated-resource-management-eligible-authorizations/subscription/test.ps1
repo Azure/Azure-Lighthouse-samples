1d941c87de4efd7b9c23b77223c3493ee845c666
Meagan Olsen <meolsen@microsoft.com>
2021-08-05 16:54:58 -0700

    Removing pii from templates and default values

--- templates/delegated-resource-management-eligible-authorizations/subscription/test.ps1
+++
@@ -1,7 +0,0 @@
-Connect-AzAccount
-Select-AzSubscription 56daa281-46ad-460f-93e6-ee48bf8facec
-New-AzSubscriptionDeployment -Name pimapproversdepl `
-                 -Location centralus `
-                 -TemplateFile "'C:\Users\meolsen\OneDrive - Microsoft\Documents\LH_test\PIM-approvers-sub\delegatedResourceManagement-eligible-authorizations-managing-tenant-approvers.json'" `
-                 -TemplateParameterFile "'C:\Users\meolsen\OneDrive - Microsoft\Documents\LH_test\PIM-approvers-sub\delegatedResourceManagement-eligible-authorizations-managing-tenant-approvers.parameters.json'" `
-                 -Verbose
